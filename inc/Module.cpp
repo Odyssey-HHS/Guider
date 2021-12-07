cd48c29e056e5619d7acabfd1fb43d26e3446a9a
Wouter de Bruijn <git@rl.hedium.nl>
2021-12-07 18:50:26 +0000

    :sparkles: Added basic Module class

---
+++ inc/Module.cpp
@@ -0,0 +1,12 @@
+#include "Module.h"
+
+Module::Module(Client client) : connection(client){};
+
+void Module::fetch()
+{
+    this->connection.send(this->getOutputsJSON());
+    char buffer[256] = {0};
+    this->connection.receive(buffer, 256);
+
+    this->setInputsJSON(buffer);
+}
---
+++ inc/Module.h
@@ -0,0 +1,15 @@
+#include "Client.h"
+
+class Module
+{
+public:
+    Module(Client client);
+    virtual char *getOutputsJSON() const = 0;
+    virtual void setInputsJSON(const char *json) = 0;
+
+    /* Sync this object with the Wemos module. Writes to the outputs and reads from the inputs of the Wemos */
+    void fetch();
+
+private:
+    Client connection;
+};
