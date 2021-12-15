48214899de80a75a2abace390bf025c20b1cc8ea
Wouter de Bruijn <wouter@hedium.nl>
2021-12-15 19:50:55 +0100

    :art: Formatted with vscode

--- inc/Bed.h
+++ inc/Bed.h
@@ -6,13 +6,13 @@ class Bed : public Module
 private:
   bool Led;
   bool Switch;
-  int DB; //druk sensor dit is analoog
+  int DB; // druk sensor dit is analoog
 public:
   Bed();
   Bed(Client client);
-  int setLed();    //this will set the status of the led
+  int setLed();    // this will set the status of the led
   int getSwitch(); // this shows the current status of the switch
-  int getDB();     //this shows the output value of the druksensor
+  int getDB();     // this shows the output value of the druksensor
 
   void setInputsJSON(const std::string json);
   std::string getOutputsJSON() const;
