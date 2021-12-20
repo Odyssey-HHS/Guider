83369c6104b5c0a309aebc34c68925b9e02f0ecc
Casper Bastiaan Net <casper.bastiaan.net@gmail.com>
2021-12-20 11:02:44 +0100

    :bug: fixed compiler issues

--- inc/Timer.cpp
+++ inc/Timer.cpp
@@ -1,26 +1,22 @@
 #include "Timer.h"
 
-class Timer 
-{
-    Timer::Timer(int stdDuration): duration(stdDuration);
+Timer::Timer(int stdDuration): duration(stdDuration){}
 
-    void Timer::start(int newDuration) {
-        if (newDuration > 0){
-            duration = int;
-        }
-        this.start();
+void Timer::start(int newDuration) {
+    if (newDuration > 0){
+        duration = newDuration;
     }
+    start();
+}
 
-    void Timer::start(){
-        timestamp = std::time(nullptr)+duration;
-    }
+void Timer::start(){
+    timestamp = std::time(nullptr)+duration;
+}
 
-    void Timer::stop(){
-        timestamp = 0;
-    }
+void Timer::stop(){
+    timestamp = 0;
+}
 
-    bool Timer::finished(){
-        return std::time(nullptr) > timestamp;
-    }
-
+bool Timer::finished(){
+    return std::time(nullptr) > timestamp;
 }
