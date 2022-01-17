200353c1b861c0a0146c0bbe6aeaf5fa8b75ca40
MauriceHBO <maurice.tacx@gmail.com>
2022-01-17 14:36:41 +0100

    final

--- src/Guider.cpp
+++ src/Guider.cpp
@@ -1,7 +1,7 @@
-#define USE_DOOR
-#define USE_CHAIR
-#define USE_BED
-#define USE_TABLELAMP
+//#define USE_DOOR
+//#define USE_CHAIR
+//#define USE_BED
+//#define USE_TABLELAMP
 #define USE_WALL
 
 #define DASHBOARD_PORT
