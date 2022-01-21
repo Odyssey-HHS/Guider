de3fe22c3cb2a0ac840730b9a83bd108bd07e6fd
Casper Bastiaan Net <casper.bastiaan.net@gmail.com>
2022-01-21 12:36:07 +0100

    :bug: Added Column

--- inc/enableModules.h
+++ inc/enableModules.h
@@ -1,5 +1,6 @@
-// #define USE_DOOR
-// #define USE_CHAIR
-// #define USE_BED
+#define USE_DOOR
+#define USE_CHAIR
+#define USE_BED
 #define USE_TABLELAMP
-// #define USE_WALL
+#define USE_WALL
+#define USE_COLUMN
